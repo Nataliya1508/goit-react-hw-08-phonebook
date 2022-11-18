13774bc653d723d6773d9f5889d55d64d55cafd6
Nataliya1508 <97055353+Nataliya1508@users.noreply.github.com>
2022-11-18 21:06:22 +0000

    Deploying to gh-pages from @ Nataliya1508/goit-react-hw-08-phonebook@3de60c7572e2a3888190beeb9e4736ad9b72696e 🚀

---
+++ static/js/657.0cdadc7e.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[657],{8657:function(e,o,n){n.r(o);n(2791);var r=n(184);o.default=function(){return(0,r.jsx)("div",{children:(0,r.jsx)("h1",{children:"Register and create your phonebook "})})}}}]);
+//# sourceMappingURL=657.0cdadc7e.chunk.js.map
